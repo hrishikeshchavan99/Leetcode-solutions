15a86bbff95c78bbc581e2ac4abc59e516a9792f
hrishikeshchavan99 <50497394+hrishikeshchavan99@users.noreply.github.com>
2022-12-04 20:59:43 +0530

    Time: 89 ms (5.14%), Space: 42.8 MB (60.00%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int count = 0, m = grid.length, n = grid[0].length;
+        Queue <Integer> queue = new LinkedList();
+        int fresh = 0;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (grid[i][j] == 1) fresh++;
+                if (grid[i][j] == 2) queue.add(i * n + j);
+            }    
+        }
+        int[][] directions = {{-1,0}, {0, -1}, {1, 0}, {0, 1}};
+        while (!queue.isEmpty() && fresh > 0){
+            System.out.println("size" + queue.size());
+            System.out.println(fresh);
+            int size = queue.size();
+            for (int k = 0; k < size; k++){
+                int rotten = queue.poll();
+                //System.out.println("rottenIndex: " + rotten);
+                int i = rotten / n;
+                int j = rotten % n;
+                for (int[] direction: directions){
+                    int x = i + direction[0];
+                    int y = j + direction[1];
+                    if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 1){
+                        System.out.println("Here " + rotten + " fresh " + fresh );
+                        queue.add(x * n + y);
+                        fresh--;
+                        grid[x][y] = 2;
+                        
+                    }
+                }
+            }
+            System.out.println("start");
+            for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+               
+                System.out.println(grid[i][j]);
+            }
+            }
+            count++;
+        }
+        //System.out.println(count);
+        return fresh == 0 ? count : -1;
+    }
+    
+}
