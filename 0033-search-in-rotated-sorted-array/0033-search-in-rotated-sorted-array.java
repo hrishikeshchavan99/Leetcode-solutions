a806b540643e6e367b567ca1d08a66397cea15d4
hrishikeshchavan99 <50497394+hrishikeshchavan99@users.noreply.github.com>
2022-12-04 23:54:05 +0530

    Time: 1 ms (80.27%), Space: 42.1 MB (80.30%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
         int low = 0, high = nums.length-1;
-        while (low < high){
+        while (low <= high){
             int mid = (low+high)/2;
             if (nums[mid] == target) return mid;
             
@@ -17,7 +17,7 @@ class Solution {
                 else
                     high = mid - 1;
        }
-        return nums[low] == target ? low : -1;
+        return -1;
         
     }
 }
