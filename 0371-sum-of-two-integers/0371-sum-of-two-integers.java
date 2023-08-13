9c63a0fd68e3c5f26286c8553e22b2a9fac90016
hrishikeshchavan99 <50497394+hrishikeshchavan99@users.noreply.github.com>
2023-08-13 10:15:37 +0530

    Time: 0 ms (100%), Space: 39.2 MB (70.46%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,8 +1,11 @@
 class Solution {
+    //a&b gives carry, a^b gives sum
+    //left shift carry by 1 and add to sum everytime
+    //carry needs to be calculated separately as XOR doesn't care of carry(1^1 = 0)
     public int getSum(int a, int b) {
         int carry;
         while (b!=0){
-            carry = a&b;
+            carry = a&b; 
             a = a^b;
             b = (carry << 1);
         }
