a0ec3971d4f704af5b04cf6543c9f50ec5053690
Hrishikesh Chavan <50497394+hrishikeshchavan99@users.noreply.github.com>
2023-08-16 08:28:52 +0530

    Time: 34 ms (16.49%), Space: 46.6 MB (69.55%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,15 +1,18 @@
 class Solution {
+    //BFS/ Dijkastra's Alogorithm
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, Map<Integer, Integer>> map = new HashMap();
         boolean[] visited = new boolean[n+1];
         int res = 0;
+        //Put all data in hashmap
         for (int[] time: times){
             map.putIfAbsent(time[0], new HashMap());
             map.get(time[0]).put(time[1], time[2]);
         }
 
+        //priority queue sorted by dist
         PriorityQueue<int[]> q = new PriorityQueue<>((a,b)-> (a[0]-b[0]));
-        q.offer(new int[]{0, k});
+        q.offer(new int[]{0, k}); // start with node k
 
         while (!q.isEmpty()){
             int[] node = q.poll();
@@ -17,9 +20,10 @@ class Solution {
             int curNode = node[1];
             if (visited[curNode]) continue;
             visited[curNode] = true;
-            n--;
-            res = curDist;
+            n--; //when n becomes 0, all nodes are visited
+            res = curDist; //distance travelled to reach that node. if that node is last one, we got the answer
             if (map.containsKey(curNode)){
+                //get all nodes connected to curNode and put into queue
                 for (int next: map.get(curNode).keySet()){
                     q.offer(new int[]{curDist+map.get(curNode).get(next), next});
                 }
