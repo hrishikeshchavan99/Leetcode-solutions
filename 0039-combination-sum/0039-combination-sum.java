a16566e840894321b3aaa5ef44ea517b6955e83f
Hrishikesh Chavan <50497394+hrishikeshchavan99@users.noreply.github.com>
2025-07-02 08:40:40 +0530

    Time: 2 ms (83.07%), Space: 44.8 MB (46.3%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,19 +1,21 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        //Arrays.sort(candidates);
-        findCombinations(0, target, candidates, ans, new ArrayList<>());
-        return ans;
+        List<List<Integer>> res = new ArrayList();
+        findCombi(candidates, target, 0, new ArrayList<Integer>(), res);
+        return res;
     }
-    private void findCombinations(int ind, int target, int[] candidates, List<List<Integer>> ans, List<Integer> temp){
-        if (target < 0) return;
-        else if (target == 0) ans.add(new ArrayList<>(temp));
-        else{
-            for (int i = ind; i < candidates.length; i++){
-                temp.add(candidates[i]);
-                findCombinations(i, target - candidates[i], candidates, ans, temp);
-                temp.remove(temp.size()-1);
-            }
+    private void findCombi(int[] candidates, int target, int idx, List<Integer> temp ,List<List<Integer>> res){
+        if (target < 0 || idx >= candidates.length) return;
+        if (target == 0){
+            res.add(new ArrayList(temp));
+            return;
         }
+
+        temp.add(candidates[idx]);
+        findCombi(candidates, target-candidates[idx], idx, temp, res);
+        temp.remove(temp.size()-1);
+
+        findCombi(candidates, target, idx+1, temp, res);
+
     }
 }
