8b9219c4e71e8648e222fa56c39a5e6707438fe6
Hrishikesh Chavan <50497394+hrishikeshchavan99@users.noreply.github.com>
2025-07-12 21:02:52 +0530

    Time: 2 ms (76.15%), Space: 63.6 MB (23.22%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -2,17 +2,16 @@ class Solution {
     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
         int events = startTime.length;
         int[] gaps = new int[events+1];
-        for (int i = 0; i < events; i++){
-            gaps[i] = i == 0 ? startTime[i] : startTime[i] - endTime[i-1];
-            //System.out.println(gaps[i]);
-        }
 
+        gaps[0] = startTime[0];
+        for (int i = 1; i < events; i++){
+            gaps[i] = startTime[i] - endTime[i-1];
+        }
         gaps[events] = eventTime - endTime[events-1];
 
         int sum = 0, max = 0;
         for (int i = 0; i < gaps.length; i++){
             sum += gaps[i];
-            //System.out.println(sum);
             if (i > k){
                 sum -= gaps[i-k-1];
             }
